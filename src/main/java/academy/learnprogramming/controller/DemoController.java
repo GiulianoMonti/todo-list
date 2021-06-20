b46cf11b1143b903523de6fa9e3614177959428e
GiulianoMonti <giulianm@gmail.com>
2021-06-20 18:06:20 -0300

    Model and Model Attributes

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -1,9 +1,13 @@
 package academy.learnprogramming.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -11,8 +15,24 @@ public class DemoController {
     // http://localhost:8080/todo-list/hello
     @ResponseBody
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+    // http://localhost:8080/todo-list/welcome
+
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        model.addAttribute("user", "Giuliano");
+        log.info("model= {}", model);
+        // prefix + name + suffix`
+        // /WEB-INF/view/welcome.jsp
+        return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcome() called");
+        return "Welcome to this Demo application.";
     }
 
 }
