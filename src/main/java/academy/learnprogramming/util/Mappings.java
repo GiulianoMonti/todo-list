e1e40fff87d7e3a314371aff0a387c112e23121e
GiulianoMonti <giulianm@gmail.com>
2021-06-23 18:27:29 -0300

    creating ToDo item Controller

---
+++ src/main/java/academy/learnprogramming/util/Mappings.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public class Mappings {
+
+    // == constants ==
+    public static final String ITEMS ="items";
+    // == constructors ==
+    private Mappings(){}
+
+
+}
